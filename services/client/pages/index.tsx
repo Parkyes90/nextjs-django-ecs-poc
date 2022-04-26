a7012adcb8dccd353d5229662293761c27dbdf38
parkyes90 <parkyes90@gmail.com>
2022-04-26 20:36:25 +0900

    Add serverSideProps

--- services/client/pages/index.tsx
+++ services/client/pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 
 const Home: NextPage = () => {
   return <div>Test</div>;
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {},
+  };
+};
