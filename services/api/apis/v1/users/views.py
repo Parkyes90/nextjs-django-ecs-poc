9b38f3de59c883a6db85f387db0ba8611856e226
parkyes90 <parkyes90@gmail.com>
2022-05-07 21:45:47 +0900

    Add api typing

--- services/api/apis/v1/users/views.py
+++ services/api/apis/v1/users/views.py
@@ -1,7 +1,22 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework import serializers
 
 
+class UserListSerializer(serializers.Serializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+    name = serializers.CharField()
+
+
+@extend_schema(
+    responses={200: UserListSerializer},
+)
 class UserListView(ListAPIView):
     def list(self, request, *args, **kwargs):
-        return Response(data={'users': ['a', 'b', 'c']})
+        return Response(data=[{"name": 1}])
--- services/api/config/settings/dev.py
+++ services/api/config/settings/dev.py
@@ -17,5 +17,8 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'DEMO PROJECT',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
     # OTHER SETTINGS
 }
